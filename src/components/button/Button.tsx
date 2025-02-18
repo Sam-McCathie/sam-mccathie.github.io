7c560ada8e68f6a68d9100256c55beade54b332c
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-02-18 17:44:14 +1300

    Update button to accept SVG's

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,34 @@
 import * as React from "react";
 import "./Button.css";
 
-interface ButtonProps {
-  text?: string; // make a conditional type, either text or svg
+/**
+ * Props for Button component when text is used.
+ * @property {never} [svg] - SVG should not be provided when text is used.
+ */
+interface ButtonTextProps {
+  text: string;
+  svg?: never;
   onClick: () => void;
 }
 
-export const Button: React.FC<ButtonProps> = ({ onClick, text }) => (
-  <button onClick={onClick}>{text}</button>
+/**
+ * Props for Button component when svg is used.
+ * @property {never} [text] - Text should not be provided when svg is used.
+ */
+interface ButtonSvgProps {
+  text?: never;
+  svg: React.ReactNode;
+  onClick: () => void;
+}
+
+type ButtonProps = ButtonTextProps | ButtonSvgProps;
+
+/**
+ * Button component that can either display text or an SVG, but not both.
+ */
+export const Button: React.FC<ButtonProps> = ({ onClick, text, svg }) => (
+  <button onClick={onClick}>
+    {text}
+    {svg}
+  </button>
 );
