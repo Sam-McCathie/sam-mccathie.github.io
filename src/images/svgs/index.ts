06e9dda1817429a5714b29daa529e717c3efe8dc
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-04-15 15:31:28 +1200

    Add new svgs

--- src/images/svgs/index.ts
+++ src/images/svgs/index.ts
@@ -2,3 +2,6 @@ export { LinkedInSVG } from "./LinkedInSVG";
 export { SunSVG } from "./SunSVG";
 export { MoonSVG } from "./MoonSVG";
 export { HamburgerSVG } from "./HamburgerSVG";
+export { DownloadSVG } from "./DownloadSVG";
+export { EmailSVG } from "./EmailSVG";
+export { GitHubSVG } from "./GitHubSVG";
