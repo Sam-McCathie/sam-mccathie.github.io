1e03b5e8fe1feb5ba19d651abb7c94b680121f7e
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-04-17 15:21:01 +1200

    Add dynamic profile switch

--- src/components/about/components/Profile.tsx
+++ src/components/about/components/Profile.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { LogoLinks } from "./LogoLinks";
+import { useTheme } from "@context";
 
 export const Profile = () => {
+  const { theme } = useTheme();
+
+  // AI generate a better dark mode - ran out of tokens :(
+  // generate the shirt colour change and swap the faces
+  const profileTheme = theme === "dark" ? "/profile-dark.jpg" : "./profile.jpg";
+
   return (
     <div className="profile">
       <LogoLinks />
       <div className="frame">
-        <img src="/profile.jpg" alt="profile picture" />
+        <img src={profileTheme} alt="profile picture" />
       </div>
     </div>
   );
