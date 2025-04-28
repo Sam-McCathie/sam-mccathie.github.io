9cc989b6fff8b97053e83858117831d125316750
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-04-28 18:37:50 +1200

    Add sticky header and adjust scroll offset

--- src/components/header/helpers/handleScroll.ts
+++ src/components/header/helpers/handleScroll.ts
@@ -1,7 +1,7 @@
 export const handleScroll = (sectionId: string) => () => {
   const section = document.getElementById(sectionId);
   if (section) {
-    const offset = 10;
+    const offset = 70;
     const sectionTop = section.getBoundingClientRect().top + window.scrollY;
     window.scrollTo({
       top: sectionTop - offset,
