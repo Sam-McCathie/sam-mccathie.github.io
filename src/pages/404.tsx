17de0c3d4b1bb50447e4ac87c74c1ba6e67cd4ad
Sam McCathie <77808434+Sam-McCathie@users.noreply.github.com>
2025-05-01 15:21:24 +1200

    Add redirect to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,13 @@
-import React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
+import { navigate } from "gatsby";
+import { useEffect } from "react";
 
-const NotFoundPage: React.FC<PageProps> = () => {
-  return (
-    <main>
-      <h1>Page not found</h1>
-      <p>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  );
+const NotFoundPage = () => {
+  useEffect(() => {
+    // Redirect to the home page on load
+    navigate("/");
+  }, []);
+
+  return null;
 };
 
 export default NotFoundPage;
-
-export const Head: HeadFC = () => <title>Not found</title>;
